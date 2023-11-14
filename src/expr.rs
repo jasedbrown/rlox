5fe81f53458f58fb54760cb0f386f5f0d0ed6758
Jason Brown <jason.b@readyset.io>
2023-11-14 09:23:07 -0800

    moved Expr evaluation back into Interpreter as we need the state from Interpreter.Environment to support variables

--- src/expr.rs
+++ src/expr.rs
@@ -1,9 +1,7 @@
 #![allow(dead_code)]
 
-use crate::rlvalue::RlValue;
-use crate::token::{Token, TokenType};
+use crate::token::Token;
 
-use anyhow::{anyhow, Result};
 use std::fmt;
 use std::fmt::{Display, Formatter};
 
@@ -76,106 +74,6 @@ impl Expr {
         s.push(')');
 
         s
-    }
-
-    pub(crate) fn evaluate(&self) -> Result<RlValue> {
-        use Expr::*;
-        match self {
-            Assign(_t, _e) => Ok(RlValue::Nil),
-            Binary(l, t, r) => {
-                let left = l.evaluate()?;
-                let right = r.evaluate()?;
-
-                match t.token_type {
-                    TokenType::Minus => {
-                        let left_d = left.as_numeric().unwrap();
-                        let right_d = right.as_numeric().unwrap();
-                        let ret_val: f64 = left_d - right_d;
-                        Ok(RlValue::Double(ret_val))
-                    }
-                    TokenType::Slash => {
-                        let left_d = left.as_numeric().unwrap();
-                        let right_d = right.as_numeric().unwrap();
-                        let ret_val: f64 = left_d / right_d;
-                        Ok(RlValue::Double(ret_val))
-                    }
-                    TokenType::Star => {
-                        let left_d = left.as_numeric().unwrap();
-                        let right_d = right.as_numeric().unwrap();
-                        let ret_val: f64 = left_d * right_d;
-                        Ok(RlValue::Double(ret_val))
-                    }
-                    TokenType::Plus => {
-                        // TODO: there's a way to do this with match ...
-                        if left.is_string() && right.is_string() {
-                            let mut ss = left.as_string().expect("must be string").clone();
-                            ss.push_str(right.as_string().expect("must be string").as_str());
-                            Ok(RlValue::String(ss))
-                        } else if left.is_numeric() && right.is_numeric() {
-                            let d = left.as_numeric().expect("nust be numeric")
-                                + right.as_numeric().expect("Must be numeric");
-                            Ok(RlValue::Double(d))
-                        } else {
-                            Err(anyhow!("mismatched types for '+' operator"))
-                        }
-                    }
-                    TokenType::Greater => {
-                        let left_d = left.as_numeric().unwrap();
-                        let right_d = right.as_numeric().unwrap();
-                        Ok(RlValue::Boolean(left_d > right_d))
-                    }
-                    TokenType::GreaterEqual => {
-                        let left_d = left.as_numeric().unwrap();
-                        let right_d = right.as_numeric().unwrap();
-                        Ok(RlValue::Boolean(left_d >= right_d))
-                    }
-                    TokenType::Less => {
-                        let left_d = left.as_numeric().unwrap();
-                        let right_d = right.as_numeric().unwrap();
-                        Ok(RlValue::Boolean(left_d < right_d))
-                    }
-                    TokenType::LessEqual => {
-                        let left_d = left.as_numeric().unwrap();
-                        let right_d = right.as_numeric().unwrap();
-                        Ok(RlValue::Boolean(left_d <= right_d))
-                    }
-                    TokenType::BangEqual => {
-                        let left_d = left.as_numeric().unwrap();
-                        let right_d = right.as_numeric().unwrap();
-                        Ok(RlValue::Boolean(left_d != right_d))
-                    }
-                    TokenType::EqualEqual => {
-                        let left_d = left.as_numeric().unwrap();
-                        let right_d = right.as_numeric().unwrap();
-                        Ok(RlValue::Boolean(left_d == right_d))
-                    }
-                    _ => Err(anyhow!("should be unreachable :(")),
-                }
-            }
-            Call(_e, _t, _v) => Ok(RlValue::Nil),
-            Get(_e, _t) => Ok(RlValue::Nil),
-            Grouping(e) => e.as_ref().evaluate(),
-            Literal(l) => Ok(RlValue::from(l)),
-            Logical(_l, _t, _r) => Ok(RlValue::Nil),
-            Set(_l, _t, _r) => Ok(RlValue::Nil),
-            Super(_t1, _t2) => Ok(RlValue::Nil),
-            This(_t) => Ok(RlValue::Nil),
-            Unary(t, e) => {
-                let right = e.evaluate()?;
-                match t.token_type {
-                    TokenType::Minus => match right.as_numeric() {
-                        Some(d) => Ok(RlValue::Double(-d)),
-                        None => Err(anyhow!("rlvalue not a numeric value")),
-                    },
-                    TokenType::Bang => {
-                        let b = !right.is_truthy();
-                        Ok(RlValue::Boolean(b))
-                    }
-                    _ => Err(anyhow!("shouldn't get here!! ....")),
-                }
-            }
-            Variable(_t) => Ok(RlValue::Nil),
-        }
     }
 }
 
--- src/interpreter.rs
+++ src/interpreter.rs
@@ -32,17 +32,17 @@ impl Interpreter {
     fn execute(&self, stmt: &Stmt) -> Result<()> {
         match stmt {
             Stmt::Print(e) => {
-                let val = self.evaluate(e)?;
+                let val = self.evaluate_expr(e)?;
                 println!("{}", val);
                 Ok(())
             }
             Stmt::Expression(e) => {
-                let _ = self.evaluate(e)?;
+                let _ = self.evaluate_expr(e)?;
                 Ok(())
             }
             Stmt::Var { name, initializer } => {
                 let val = match initializer {
-                    Some(e) => Some(self.evaluate(e)?),
+                    Some(e) => Some(self.evaluate_expr(e)?),
                     None => None,
                 };
                 self.environment.define(name.lexeme.clone(), val);
@@ -52,7 +52,106 @@ impl Interpreter {
         }
     }
 
-    fn evaluate(&self, expr: &Expr) -> Result<RlValue> {
-        expr.evaluate()
+    fn evaluate_expr(&self, expr: &Expr) -> Result<RlValue> {
+        use Expr::*;
+        match expr {
+            Assign(_t, _e) => Ok(RlValue::Nil),
+            Binary(l, t, r) => {
+                let left = self.evaluate_expr(l)?;
+                let right = self.evaluate_expr(r)?;
+
+                match t.token_type {
+                    TokenType::Minus => {
+                        let left_d = left.as_numeric().unwrap();
+                        let right_d = right.as_numeric().unwrap();
+                        let ret_val: f64 = left_d - right_d;
+                        Ok(RlValue::Double(ret_val))
+                    }
+                    TokenType::Slash => {
+                        let left_d = left.as_numeric().unwrap();
+                        let right_d = right.as_numeric().unwrap();
+                        let ret_val: f64 = left_d / right_d;
+                        Ok(RlValue::Double(ret_val))
+                    }
+                    TokenType::Star => {
+                        let left_d = left.as_numeric().unwrap();
+                        let right_d = right.as_numeric().unwrap();
+                        let ret_val: f64 = left_d * right_d;
+                        Ok(RlValue::Double(ret_val))
+                    }
+                    TokenType::Plus => {
+                        // TODO: there's a way to do this with match ...
+                        if left.is_string() && right.is_string() {
+                            let mut ss = left.as_string().expect("must be string").clone();
+                            ss.push_str(right.as_string().expect("must be string").as_str());
+                            Ok(RlValue::String(ss))
+                        } else if left.is_numeric() && right.is_numeric() {
+                            let d = left.as_numeric().expect("nust be numeric")
+                                + right.as_numeric().expect("Must be numeric");
+                            Ok(RlValue::Double(d))
+                        } else {
+                            Err(anyhow!("mismatched types for '+' operator"))
+                        }
+                    }
+                    TokenType::Greater => {
+                        let left_d = left.as_numeric().unwrap();
+                        let right_d = right.as_numeric().unwrap();
+                        Ok(RlValue::Boolean(left_d > right_d))
+                    }
+                    TokenType::GreaterEqual => {
+                        let left_d = left.as_numeric().unwrap();
+                        let right_d = right.as_numeric().unwrap();
+                        Ok(RlValue::Boolean(left_d >= right_d))
+                    }
+                    TokenType::Less => {
+                        let left_d = left.as_numeric().unwrap();
+                        let right_d = right.as_numeric().unwrap();
+                        Ok(RlValue::Boolean(left_d < right_d))
+                    }
+                    TokenType::LessEqual => {
+                        let left_d = left.as_numeric().unwrap();
+                        let right_d = right.as_numeric().unwrap();
+                        Ok(RlValue::Boolean(left_d <= right_d))
+                    }
+                    TokenType::BangEqual => {
+                        let left_d = left.as_numeric().unwrap();
+                        let right_d = right.as_numeric().unwrap();
+                        Ok(RlValue::Boolean(left_d != right_d))
+                    }
+                    TokenType::EqualEqual => {
+                        let left_d = left.as_numeric().unwrap();
+                        let right_d = right.as_numeric().unwrap();
+                        Ok(RlValue::Boolean(left_d == right_d))
+                    }
+                    _ => Err(anyhow!("should be unreachable :(")),
+                }
+            }
+            Call(_e, _t, _v) => Ok(RlValue::Nil),
+            Get(_e, _t) => Ok(RlValue::Nil),
+            Grouping(e) => self.evaluate_expr(e.as_ref()),
+            Literal(l) => Ok(RlValue::from(l)),
+            Logical(_l, _t, _r) => Ok(RlValue::Nil),
+            Set(_l, _t, _r) => Ok(RlValue::Nil),
+            Super(_t1, _t2) => Ok(RlValue::Nil),
+            This(_t) => Ok(RlValue::Nil),
+            Unary(t, e) => {
+                let right = self.evaluate_expr(e)?;
+                match t.token_type {
+                    TokenType::Minus => match right.as_numeric() {
+                        Some(d) => Ok(RlValue::Double(-d)),
+                        None => Err(anyhow!("rlvalue not a numeric value")),
+                    },
+                    TokenType::Bang => {
+                        let b = !right.is_truthy();
+                        Ok(RlValue::Boolean(b))
+                    }
+                    _ => Err(anyhow!("shouldn't get here!! ....")),
+                }
+            }
+            Variable(t) => match self.environment.get(t)? {
+                Some(rlvalue) => Ok(rlvalue),
+                None => Ok(RlValue::Nil),
+            },
+        }
     }
 }
